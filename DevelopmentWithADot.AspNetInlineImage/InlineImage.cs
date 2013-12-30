ef9bd6ed61af067d83da69e752907fa029377911
Ricardo Peres <rjperes@hotmail.com>
2013-12-30 15:05:59 +0000

    Initial version

---
+++ DevelopmentWithADot.AspNetInlineImage/InlineImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Web.UI.WebControls;
+
+namespace DevelopmentWithADot.AspNetInlineImage
+{
+	public class InlineImage : Image
+	{
+		#region Protected override methods
+		protected override void OnInit(EventArgs e)
+		{
+			String imagePath = this.Context.Server.MapPath(this.ImageUrl);
+			String extension = Path.GetExtension(imagePath).Substring(1);
+			Byte[] imageData = File.ReadAllBytes(imagePath);
+
+			this.ImageUrl = String.Format("data:image/{0};base64,{1}", extension, Convert.ToBase64String(imageData));
+
+			base.OnInit(e);
+		}
+		#endregion
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+DevelopmentWithADot.InlineImage
+===============================
+
+ASP.NET inline image control
+First discussed on http://weblogs.asp.net/ricardoperes/archive/2011/11/30/inline-image-in-asp-net.aspx.
+
+Copyright Ricardo Peres 2013
